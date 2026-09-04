ffbdf9759976610c069a2a1d28a3b75ece42f596
Daniel <daniel.lukins1@gmail.com>
2026-09-04 17:26:55 +1000

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1]);
+
+    let path: &String = if args.len() > 1 {
+        &args[1]
+    } else {
+        &String::from(".")
+    };
 }
